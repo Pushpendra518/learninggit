6e07981f8567eebcd361bdc035a0c2db30af762b
Pushpendra Menaria <menariapushpendra164@gmail.com>
2024-08-12 10:18:53 +0530

    msg

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log("gi version1");
-console.log("gi version new line");
+console.log("git version1");
+console.log("git version new line");
+
+
+
+
+//git init
+//git status
+//git add script.js
+//git status
+//git config --global user.email "menariapushpendra164@gmail.com"
+//git config --global user.name "Pushpendra Menaria" 
+//git commit -m "initialCommit"
