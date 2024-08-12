2004d9c8ba7b856df5b9fb9ad6adcd3fad526721
Pushpendra Menaria <menariapushpendra164@gmail.com>
2024-08-12 10:27:17 +0530

    msg3

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 console.log("git version1");
-console.log("git version new line");
+console.log("git version new line version3");
 
 
 
 
 //git init
 //git status
-//git add script.js
+//git add script.js      //add new file
 //git status
 //git config --global user.email "menariapushpendra164@gmail.com"
 //git config --global user.name "Pushpendra Menaria" 
 //git commit -m "initialCommit"
+//git log --all
+//git status
+//git add .              //for change in file and resave
+//git status 
+//git commit -m "msg"    //create version
+//git log --all           
+//git commit -m "msg1"    //create new version
+//git log --all
+//git checkout 6e07981f8567eebcd361bdc035a0c2db30af762b        //for change version 
+//git checkout 272d4104a71521e07e5d0b25b62e8dfb40f33bec        //for change previous version
